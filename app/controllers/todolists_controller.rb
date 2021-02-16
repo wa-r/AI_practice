3d6ae5ba166e0547447e489093a5236d8082b867
wa-r <nyuuuuun14@gmail.com>
2021-02-16 09:26:12 +0000

    [追加] Natural Language API機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,9 +1,12 @@
 class TodolistsController < ApplicationController
+
   def new
     @list = List.new
   end
+
   def create
     list = List.new(list_params)
+    list.score = Language.get_data(list_params[:body])
     list.save
     tags = Vision.get_image_data(list.image)
     tags.each do |tag|
@@ -11,20 +14,25 @@ class TodolistsController < ApplicationController
     end
     redirect_to todolist_path(list.id)
   end
+
   def index
     @lists = List.all
   end
+
   def show
     @list = List.find(params[:id])
   end
+
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
   def destroy
     list = List.find(params[:id])
     list.destroy
@@ -35,4 +43,5 @@ class TodolistsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
+
 end
